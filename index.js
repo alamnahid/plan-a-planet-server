8a7fa77f43b3b99c5128128a1ef5808eb26c5932
alamnahid <nahidalam13765@gmail.com>
2023-11-01 20:37:41 +0600

    implement jwt to verify

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
+var jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000
 //middle ware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true
+  }));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -23,11 +29,58 @@ const client = new MongoClient(uri, {
     }
 });
 
+// middlewares 
+const logger = (req, res, next) =>{
+    console.log('log: info', req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) =>{
+    const token = req?.cookies?.token;
+    // console.log('token in the middleware', token);
+    // no token available 
+    if(!token){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+            return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
 
         const database = client.db("plantDB");
         const plantCollection = database.collection("plants")
+        const cartcollection = database.collection("cart")
+
+        // auth related apis
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body;
+            console.log('user for token', user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            })
+                .send({ success: true });
+        })
+
+        app.post('/logout', async (req, res) => {
+            const user = req.body;
+            console.log('logging out', user);
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+        })
+
+
+
+
 
         app.post('/plants', async (req, res) => {
             const plant = req.body;
@@ -73,6 +126,39 @@ async function run() {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
             const result = await plantCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        // cart collection 
+
+        app.post('/cart', async (req, res) => {
+            const cart = req.body;
+            console.log(cart)
+            const result = await cartcollection.insertOne(cart);
+            res.send(result)
+        })
+
+        app.get('/cart', logger, verifyToken, async(req, res)=>{
+            console.log(req.query.email);
+                 // console.log('ttttt token', req.cookies.token)
+                 console.log('user in the valid token', req.user)
+                 if(req.query.email !== req.user.email){
+                     return res.status(403).send({message: 'forbidden access'})
+                 }
+     
+                 let query = {};
+                 if (req.query?.email) {
+                     query = { email: req.query.email }
+                 }
+           const cursor = cartcollection.find(query);
+           const result = await cursor.toArray();
+           res.send(result)
+         })
+
+        app.delete('/cart/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await cartcollection.deleteOne(query)
             res.send(result)
         })
 
